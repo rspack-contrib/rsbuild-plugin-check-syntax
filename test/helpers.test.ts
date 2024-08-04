6938d4d02283fc99f285101823e60d0ffd0af49f
chenjiahan <chenjiahan.jait@bytedance.com>
2024-08-04 11:05:43 +0800

    test: fix snapshots

--- test/helpers.test.ts
+++ test/helpers.test.ts
@@ -27,7 +27,13 @@ test('#getHtmlScripts - should extract inline scripts correctly', async () => {
       </script>
     </body>
   </html>`).join('\n'),
-  ).toMatchSnapshot();
+  ).toEqual(`console.log('Inline script 1');
+console.log('Inline script 2');
+console.log('Inline script 3');
+var message = \"This is a test script.\";
+        console.log(message);
+        alert(\"This is an alert.
+It has a line break.\");`);
 });
 
 test('#getHtmlScripts - should not extract external scripts and JSON scripts', async () => {
@@ -58,12 +64,25 @@ test('#makeCodeFrame = should make code frame correctly', () => {
     'var c = 3;',
   ];
 
-  expect(makeCodeFrame(lines, 0)).toMatchSnapshot();
-  expect(makeCodeFrame(lines, 4)).toMatchSnapshot();
+  expect(makeCodeFrame(lines, 0)).toEqual(`
+[33m   > 1 | const a = 1;[39m
+[90m     2 | [39m
+[90m     3 | var b = 2;[39m
+[90m     4 | [39m`);
+  expect(makeCodeFrame(lines, 4)).toEqual(`
+[90m     2 | [39m
+[90m     3 | var b = 2;[39m
+[90m     4 | [39m
+[33m   > 5 | console.log(() => {[39m
+[90m     6 |   return a + b;[39m
+[90m     7 | });[39m
+[90m     8 | [39m`);
 });
 
 test('#displayCodePointer - should display code pointer correctly', () => {
   const code =
     '(self.webpackChunktmp=self.webpackChunktmp||[]).push([[179],{530:()=>{console.log(1);let e=1;e="2"}},e=>{var l;l=530,e(e.s=l)}]);';
-  expect(`\n  code:    ${displayCodePointer(code, 66)}`).toMatchSnapshot();
+  expect(`\n  code:    ${displayCodePointer(code, 66)}`).toEqual(`
+  code:    .webpackChunktmp||[]).push([[179],{530:()=>{console.log(1);let e=1;e="2"}},e=>{v
+[33m                                                  ^[39m`);
 });
