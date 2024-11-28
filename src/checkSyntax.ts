06f65c6cb0ed7ba7562b46abb407604f0dad0a12
nyqykk <1327719263@qq.com>
2024-11-28 15:10:26 +0800

    chore: export checkSyntax as utils

---
+++ src/checkSyntax.ts
@@ -0,0 +1,87 @@
+import fs from 'node:fs';
+import { parse } from 'acorn';
+import { browserslistToESVersion } from 'browserslist-to-es-version';
+import { generateError } from './generateError.js';
+import { generateHtmlScripts } from './generateHtmlScripts.js';
+import type {
+  AcornParseError,
+  CheckSyntaxExclude,
+  CheckSyntaxOptions,
+  ECMASyntaxError,
+  EcmaVersion,
+} from './types.js';
+import { checkIsExclude } from './utils.js';
+
+const HTML_REGEX = /\.html$/;
+export const JS_REGEX: RegExp = /\.(?:js|mjs|cjs|jsx)$/;
+
+export class CheckSyntax {
+  errors: ECMASyntaxError[] = [];
+
+  ecmaVersion: EcmaVersion;
+
+  targets: string[];
+
+  rootPath: string;
+
+  exclude: CheckSyntaxExclude | undefined;
+
+  excludeOutput: CheckSyntaxExclude | undefined;
+
+  constructor(
+    options: CheckSyntaxOptions &
+      Required<Pick<CheckSyntaxOptions, 'targets'>> & {
+        rootPath: string;
+      },
+  ) {
+    this.targets = options.targets;
+    this.exclude = options.exclude;
+    this.excludeOutput = options.excludeOutput;
+    this.rootPath = options.rootPath;
+    this.ecmaVersion =
+      options.ecmaVersion || browserslistToESVersion(this.targets);
+  }
+
+  async check(filepath: string, code?: string) {
+    // If the code is provided, no need to read file.
+    if (code) {
+      return await this.tryParse(filepath, code);
+    }
+
+    if (HTML_REGEX.test(filepath)) {
+      const htmlScripts = await generateHtmlScripts(filepath);
+      await Promise.all(
+        htmlScripts.map(async (script) => {
+          if (!checkIsExclude(filepath, this.exclude)) {
+            await this.tryParse(filepath, script);
+          }
+        }),
+      );
+    }
+
+    if (JS_REGEX.test(filepath)) {
+      const jsScript = await fs.promises.readFile(filepath, 'utf-8');
+      await this.tryParse(filepath, jsScript);
+    }
+  }
+
+  async tryParse(filepath: string, code: string) {
+    try {
+      parse(code, { ecmaVersion: this.ecmaVersion });
+    } catch (_: unknown) {
+      const err = _ as AcornParseError;
+
+      const error = await generateError({
+        err,
+        code,
+        filepath,
+        exclude: this.exclude,
+        rootPath: this.rootPath,
+      });
+
+      if (error) {
+        this.errors.push(error);
+      }
+    }
+  }
+}
